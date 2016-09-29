d0f122a1bd3aba678715af0b5906e38ceb660a2f
nocotan <noconoco.lib@gmail.com>
2016-09-29 15:46:24 +0900

    graph api

--- src/bot/mod.rs
+++ src/bot/mod.rs
@@ -1,14 +1,18 @@
 extern crate hyper;
+extern crate multipart;
 
+use self::hyper::client::Request;
 use self::hyper::client::Response;
+use self::hyper::method::Method;
+use self::hyper::net::Streaming;
 
-mod graph_api;
-mod utils;
+use self::multipart::client::Multipart;
 
 
 pub struct Bot {
     access_token: String,
     app_secret: String,
+    graph_url: String,
 }
 
 impl Bot {
@@ -16,21 +20,24 @@ impl Bot {
         Bot {
             access_token: access_token.to_string(),
             app_secret: app_secret.to_string(),
+            graph_url: "https://graph.facebook.com/v2.6".to_string(),
         }
     }
 
-    pub fn send_raw(self, payload: &str) -> String {
-        let root_api = graph_api::FacebookGraphApi::new(self.access_token, self.app_secret);
-        let api = graph_api::FacebookGraphApi{..root_api};
+    pub fn send_raw(self, payload: &str) {
+        let mut request_endpoint = format!("{}{}", self.graph_url, "/me/messages");
 
-        let request_endpoint = api.auth_url();
+        println!("{}", request_endpoint);
 
-        let request = utils::UrlRequest::new();
+        let request = Request::new(Method::Post,
+                                   request_endpoint.to_string().parse().expect("Failed")
+                                   ).expect("Failed");
+        let mut multipart = Multipart::from_request(request).expect("Failed");
 
-        let access_token = api.get_access_token();
-        let body = format!("{}{}", "access_token=", access_token);
-        let response = request.post(request_endpoint, body);
+        let body = format!("{}{}", "access_token=", self.access_token);
+        let response = multipart.send().expect("Failed");
 
-        response.status.to_string()
+        println!("{}", response.status.to_string());
+
     }
 }
